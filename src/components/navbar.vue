09fe4599fb6ce7071f55c8a1b75dc61f74ad0111
AlexAnthony13003 <alexisguillermain.pro@gmail.com>
2023-03-15 16:24:50 +0100

    style

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,35 +1,50 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<div class="navbarsetc">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-black">
   <a class="navbar-brand" href="/"><img src="../assets/SwiftAndCode.jpeg" alt=""></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="presentation">Présentation<span class="sr-only">(current)</span></a>
+      <li class="nav-item" :class="{ active: isActive('/presentation') }">
+        <a class="nav-link" href="presentation">Présentation</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item" :class="{ active: isActive('/projects') }">
         <a class="nav-link" href="projects">Projets Réalisés</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item" :class="{ active: isActive('/team') }">
         <a class="nav-link" href="team">L'équipe</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item" :class="{ active: isActive('/contact') }">
         <a class="nav-link" href="contact">Contact</a>
       </li>
     </ul>
   </div>
 </nav>
+</div>
 </template>
 <script>
 export default {
 // eslint-disable-next-line vue/multi-word-component-names
   name: 'navbar',
+    methods: {
+    isActive(route) {
+      return this.$route.path === route
+    }
+  }
 }
 </script>
 <style>
-    img{
-        height: 50px;
-    }
+  .nav-item {
+    color: #ccc;
+  }
+  img{
+    height: 50px;
+  }
+
+  .active {
+    font-weight: bold;
+    color: white;
+  }
 </style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,13 +1,14 @@
 <template>
-    popo
+  <contact_form></contact_form>
 </template>
 
 <script>
 // @ is an alias to /src
+import contact_form from '@/components/contact-form.vue'
 export default {
   name: 'ContactView',
   components: {
-    
+    contact_form
   }
 }
 </script>
